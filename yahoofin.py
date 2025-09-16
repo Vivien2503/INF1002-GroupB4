8ce71da89b146ebd5e760313eff983fd7d21df56
amandadou <amandadeng9@gmail.com>
2025-09-16 16:30:49 +0800

    Update yahoofin.py

--- yahoofin.py
+++ yahoofin.py
@@ -1,9 +1,27 @@
+
+import yfinance as yf
+
+# Download SPY data
+data = yf.download('SPY', start:='2022-01-01', end='2025-01-01')
+
+# Make sure the index is a DatetimeIndex
+data.index = data.index.tz_localize(None)
+
+# ...existing code...
+year = 2024
+data_year = data[data.index.year == year]
+
 # Daily upward and downward movements
 up_runs = []
 down_runs = []
 # Current direction = current_dir
 current_dir = None
 
+year = 2024
+data_year = data[data.index.year == year]
+
+# Define 'close' as a NumPy array of closing prices
+close = data_year['Close'].values
 for i in range(1, len(close)):
     if close[i] > close[i-1]:
         if current_dir == "up":
